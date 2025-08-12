7802dc4cc221d0bee22ca2ff8be7653e4cf9a73b
Durvesh Manjhi <mdurvesh789@gmail.com>
2025-08-12 17:15:36 +0530

    start to working

---
+++ ShoppingApp/ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Durvesh Manjhi on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
